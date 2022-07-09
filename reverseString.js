0cbd4121e0c27eb1fa0ddf792170140f31fb9e15
ragini sahu <31927680+raginisahu@users.noreply.github.com>
2022-07-09 22:45:45 +0530

    Add files via upload

---
+++ reverseString.js
@@ -0,0 +1,9 @@
+const array = "abcdef"
+let reverseStr = ""
+for(let i =array.length-1; i>=0; i --) {
+	reverseStr = reverseStr + array[i]
+}
+console.log("Hey! your reverse String:", reverseStr);
+
+
+
